a8c2ddd4c2aac24aca3b7b24de22aea808cf9167
Dante Pereyra <dante@gentecnologico.com.ar>
2022-03-13 11:18:24 -0300

    fixes

--- Flask/static/menu.js
+++ Flask/static/menu.js
@@ -2,16 +2,7 @@
 let cookies = document.cookie
 let logueado = cookies.includes("usuario") && cookies.includes("validado=true")
 const nav = document.getElementById("navigation")
-
-if (logueado) {
-    nav.innerHTML += '<a href="/agregar" class="link" id="agregar">+ Agregar</a><a href="/perfil" class="link" id="perfil">Perfil</a>'
-} else {
-    nav.innerHTML += '<a href="/ingresar" class="link login">Ingresar</a>'
-}
-
-// Navegacion Mobile
-document.getElementById("mobile-hamburger").addEventListener("click", () => nav.classList.add("mobile-active"));
-document.getElementById("mobile-cerrar").addEventListener("click", () => nav.classList.remove("mobile-active"));
+let user_logueado = ""
 
 let obtenerCookie = (nombre) => {
     let array = document.cookie.split(";")
@@ -20,5 +11,16 @@ let obtenerCookie = (nombre) => {
     return c.substring(i+nombre.length+1)
 }
 
-// Usuario actualmente logueado
-let user_logueado = obtenerCookie("id")
+
+if (logueado) {
+    nav.innerHTML += '<a href="/agregar" class="link" id="agregar">+ Agregar</a><a href="/perfil" class="link" id="perfil">Perfil</a>'
+    // Usuario actualmente logueado
+    user_logueado = obtenerCookie("id")
+} else {
+    nav.innerHTML += '<a href="/ingresar" class="link login">Ingresar</a>'
+}
+
+// Navegacion Mobile
+document.getElementById("mobile-hamburger").addEventListener("click", () => nav.classList.add("mobile-active"));
+document.getElementById("mobile-cerrar").addEventListener("click", () => nav.classList.remove("mobile-active"));
+
